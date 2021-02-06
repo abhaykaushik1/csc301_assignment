5c04c6995ba422c9babcc1e8e21f2ba35c8f3727
Abhay Kaushik <abhaykaushik@outlook.com>
2021-02-06 01:30:08 -0500

    Added small updates to Add.java

--- a1/starter_code/src/main/java/ca/utoronto/utm/mcs/Add.java
+++ a1/starter_code/src/main/java/ca/utoronto/utm/mcs/Add.java
@@ -1,6 +1,9 @@
 package ca.utoronto.utm.mcs;
 
 import java.io.Exception;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Add {
     
@@ -9,8 +12,9 @@ public class Add {
             System.out.println("400 BAD REQUEST.");
             return;
         }
-        
-        
+        String tsvfile = "../../../../../../../../starter_files/movies.tsv";
+        String newMovieId = "tt"+movieId;
+        BufferedReader movies = new BufferedReader(new FileReader(tsvfile));
     }
     
     public static void main(String args[]) throws Exception {
